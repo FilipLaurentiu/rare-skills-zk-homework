45f82555e574e15ac4501f1224c8bfd41ffc3b33
Filip Laurentiu <f.laurentiu26@gmail.com>
2023-11-02 23:59:17 +0200

    Optimizations Use precompiles to calculate the inverse of a number

--- Week4/contracts/Homework3.sol
+++ Week4/contracts/Homework3.sol
@@ -13,6 +13,26 @@ contract Homework3 {
 
     ECPoint G1 = ECPoint(1, 2);
 
+    function inv_mod(uint256 n, uint256 m) public view returns (uint256 o) {
+        (bool ok, bytes memory result) = address(5).staticcall(
+            abi.encode(32, 32, 32, n, m - 2, m)
+        );
+        require(ok, "Fail");
+        return abi.decode(result, (uint256));
+    }
+
+    function ec_add(
+        ECPoint memory A,
+        ECPoint memory B
+    ) public view returns (ECPoint memory) {
+        (bool success, bytes memory result) = address(6).staticcall(
+            abi.encode(A.x, A.y, B.x, B.y)
+        );
+        require(success, "Point addition failed!");
+        ECPoint memory sum = abi.decode(result, (ECPoint));
+        return sum;
+    }
+
     function ec_multiply(
         ECPoint memory P,
         uint256 scalar
@@ -25,22 +45,10 @@ contract Homework3 {
         return res;
     }
 
-    function ec_add(
-        ECPoint memory A,
-        ECPoint memory B
-    ) public view returns (ECPoint memory) {
-        (bool success, bytes memory result) = address(0x6).staticcall(
-            abi.encode(A.x, A.y, B.x, B.y)
-        );
-        require(success, "Point addition failed!");
-        ECPoint memory sum = abi.decode(result, (ECPoint));
-        return sum;
-    }
-
     function ec_eq(
         ECPoint memory A,
         ECPoint memory B
-    ) public view returns (bool) {
+    ) public pure returns (bool) {
         return (A.x == B.x && A.y == B.y);
     }
 
@@ -53,59 +61,13 @@ contract Homework3 {
         // return true if the prover knows two numbers that add up to num/den
         ECPoint memory sum = ec_add(A, B);
 
-        uint256 den_inv = calculateModularInverse(den, bn128_curve_order);
-
+        uint256 den_inv = inv_mod(den, bn128_curve_order);
         uint256 mul = mulmod(num, den_inv, bn128_curve_order);
-
         ECPoint memory mulG1 = ec_multiply(G1, mul);
 
         require(ec_eq(sum, mulG1), "Equality fail");
 
         return true;
-    }
-
-    /// from ChatGPT
-    function calculateModularInverse(
-        uint256 num,
-        uint256 modulus
-    ) public pure returns (uint256) {
-        require(modulus > 1, "Modulus must be greater than 1");
-        require(
-            gcd(num, modulus) == 1,
-            "The number and modulus must be coprime"
-        );
-
-        int256 a = int256(num);
-        int256 m = int256(modulus);
-        int256 m0 = m;
-        int256 x0 = 0;
-        int256 x1 = 1;
-
-        while (a > 1) {
-            int256 q = a / m;
-            int256 t = m;
-            m = a % m;
-            a = t;
-
-            t = x0;
-            x0 = x1 - q * x0;
-            x1 = t;
-        }
-
-        if (x1 < 0) {
-            x1 += m0;
-        }
-
-        return uint256(x1);
-    }
-
-    function gcd(uint256 a, uint256 b) internal pure returns (uint256) {
-        while (b != 0) {
-            uint256 temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
     }
 
     function matmul(
