2b8d3b5d2e6a554abb78ff21d5164ef106de1dc9
Filip Laurentiu <f.laurentiu26@gmail.com>
2023-11-09 17:30:53 +0200

    Solution for Homework 4

---
+++ Week5/contracts/Homework4.sol
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Homework4 {
+    struct G1_ECPoint {
+        uint256 x;
+        uint256 y;
+    }
+
+    struct G2_ECPoint {
+        uint256[2] x;
+        uint256[2] y;
+    }
+
+    uint256 constant PRIME_Q =
+        21888242871839275222246405745257275088548364400416034343698204186575808495617;
+
+    G1_ECPoint G1 = G1_ECPoint(1, 2);
+
+    G1_ECPoint Alpha_G1;
+    G2_ECPoint Beta_G2;
+    G2_ECPoint Gamma_G2;
+    G2_ECPoint Delta_G2;
+
+    constructor(
+        G1_ECPoint memory alpha_G1,
+        G2_ECPoint memory beta_G1,
+        G2_ECPoint memory gamma_G2,
+        G2_ECPoint memory delta_G2
+    ) {
+        Alpha_G1 = alpha_G1;
+        Beta_G2 = beta_G1;
+        Gamma_G2 = gamma_G2;
+        Delta_G2 = delta_G2;
+    }
+
+    function inv_mod(uint256 n, uint256 m) public view returns (uint256 o) {
+        (bool ok, bytes memory result) = address(5).staticcall(
+            abi.encode(32, 32, 32, n, m - 2, m)
+        );
+        require(ok, "Fail");
+        return abi.decode(result, (uint256));
+    }
+
+    function ec_add(
+        G1_ECPoint memory A,
+        G1_ECPoint memory B
+    ) public view returns (G1_ECPoint memory) {
+        (bool success, bytes memory result) = address(6).staticcall(
+            abi.encode(A.x, A.y, B.x, B.y)
+        );
+        require(success, "Point addition failed!");
+        G1_ECPoint memory sum = abi.decode(result, (G1_ECPoint));
+        return sum;
+    }
+
+    function ec_multiply(
+        G1_ECPoint memory P,
+        uint256 scalar
+    ) public view returns (G1_ECPoint memory) {
+        (bool ok, bytes memory result) = address(7).staticcall(
+            abi.encode(P.x, P.y, scalar)
+        );
+        require(ok, "Multiplication failed");
+        G1_ECPoint memory res = abi.decode(result, (G1_ECPoint));
+        return res;
+    }
+
+    function ec_eq(
+        G1_ECPoint memory A,
+        G1_ECPoint memory B
+    ) public pure returns (bool) {
+        return (A.x == B.x && A.y == B.y);
+    }
+
+    function negate(
+        G1_ECPoint memory p
+    ) internal pure returns (G1_ECPoint memory) {
+        // The prime q in the base field F_q for G1
+        if (p.x == 0 && p.y == 0) {
+            return G1_ECPoint(0, 0);
+        } else {
+            return G1_ECPoint(p.x, PRIME_Q - (p.y % PRIME_Q));
+        }
+    }
+
+    function verify_pairing(
+        G1_ECPoint calldata neg_A_1,
+        G2_ECPoint calldata B_2,
+        G1_ECPoint calldata C_1,
+        uint256 x_1,
+        uint256 x_2,
+        uint256 x_3
+    ) public view returns (bool) {
+        G1_ECPoint memory G1 = G1;
+        G1_ECPoint memory x_1G_1 = ec_multiply(G1, x_1);
+        G1_ECPoint memory x_2G_1 = ec_multiply(G1, x_2);
+        G1_ECPoint memory x_3G_1 = ec_multiply(G1, x_3);
+        G1_ECPoint memory X_1 = ec_add(ec_add(x_1G_1, x_2G_1), x_3G_1);
+
+        uint256[24] memory input;
+
+        input[0] = neg_A_1.x;
+        input[1] = neg_A_1.y;
+
+        input[2] = B_2.x[0];
+        input[3] = B_2.x[1];
+        input[4] = B_2.y[0];
+        input[5] = B_2.y[1];
+
+        input[6] = Alpha_G1.x;
+        input[7] = Alpha_G1.y;
+
+        input[8] = Beta_G2.x[0];
+        input[9] = Beta_G2.x[1];
+        input[10] = Beta_G2.y[0];
+        input[11] = Beta_G2.y[1];
+
+        input[12] = X_1.x;
+        input[13] = X_1.y;
+
+        input[14] = Gamma_G2.x[0];
+        input[15] = Gamma_G2.x[1];
+        input[16] = Gamma_G2.y[0];
+        input[17] = Gamma_G2.y[1];
+
+        input[18] = C_1.x;
+        input[19] = C_1.y;
+
+        input[20] = Delta_G2.x[0];
+        input[21] = Delta_G2.x[1];
+        input[22] = Delta_G2.y[0];
+        input[23] = Delta_G2.y[1];
+
+        (bool success, bytes memory data) = address(8).staticcall(
+            abi.encode(input)
+        );
+        if (success) return abi.decode(data, (bool));
+
+        revert("Wrong pairing");
+    }
+}
---
+++ Week5/tests/test_Homework4.py
@@ -0,0 +1,54 @@
+from py_ecc.bn128 import G1, G2, curve_order, multiply, add, eq, neg
+import pytest
+from ape import project
+
+
+class G1_ECPoint:
+    def __init__(self, P):
+        self.x = repr(P[0])
+        self.y = repr(P[1])
+
+
+class G2_ECPoint:
+    def __init__(self, P):
+        self.x = list([repr(P[0].coeffs[1]), repr(P[0].coeffs[0])])
+        self.y = list([repr(P[1].coeffs[1]), repr(P[1].coeffs[0])])
+
+
+@pytest.fixture
+def account1(accounts):
+    return accounts[0]
+
+
+@pytest.fixture
+def homework4_contract(account1):
+    alpha_1 = 5
+    beta_2 = 6
+    gamma_2 = 3
+    delta_2 = 7
+    contract = account1.deploy(project.Homework4,
+                               G1_ECPoint(multiply(G1, alpha_1)),
+                               G2_ECPoint(multiply(G2, beta_2)),
+                               G2_ECPoint(multiply(G2, gamma_2)),
+                               G2_ECPoint(multiply(G2, delta_2)))
+    return contract
+
+
+def test_verify_pairing(homework4_contract):
+    a = 32
+    b = 4
+    c = 8
+
+    x_1 = 6
+    x_2 = 5
+    x_3 = 3
+
+    A_1 = multiply(G1, a)
+    B_2 = multiply(G2, b)
+    C_1 = multiply(G1, c)
+
+    # X_1 = 6G_1 + 5G_1 + 3G_1
+    # 0 = -A1*B2 + alpha1*beta2 + X1*gama2 + C1*delta2
+    # 0 = -32*4 + 5*6 + (6+5+3)*3 + 8*7
+    homework4_contract.verify_pairing(G1_ECPoint(
+        neg(A_1)), G2_ECPoint(B_2), G1_ECPoint(C_1), x_1, x_2, x_3)
